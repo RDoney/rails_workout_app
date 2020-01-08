754cdfcc42edb1a05a93e0a3a9087a1583f5537a
Ryan Doney <r.doney89@gmail.com>
2020-01-07 19:22:11 -0600

    add exercise model validations

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -4,13 +4,14 @@ RSpec.feature 'Creating a new exercise' do
   before do
     @john = User.create(email: "john@example.com", password: "password")
     login_as(@john)
-  end
 
-  scenario "with valid inputs" do
     visit "/"
     click_link "My Lounge"
     click_link "New Workout"
     expect(page).to have_link("Back")
+  end
+
+  scenario "with valid inputs" do
 
     fill_in "Duration", with: 70
     fill_in "Workout Details", with: "Weight lifting"
@@ -25,10 +26,6 @@ RSpec.feature 'Creating a new exercise' do
   end
 
   scenario "with invalid inputs" do
-    visit "/"
-    click_link "My Lounge"
-    click_link "New Workout"
-    expect(page).to have_link("Back")
 
     fill_in "Duration", with: ""
     fill_in "Workout Details", with: ""
