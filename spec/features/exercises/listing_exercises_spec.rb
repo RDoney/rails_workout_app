c6c1ff3fe2b371ff18117384683031b1ea0ebf1b
Ryan Doney <r.doney89@gmail.com>
2020-01-07 20:28:35 -0600

    edit exercise model default_scope to show only last 7 days

--- spec/features/exercises/listing_exercises_spec.rb
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -7,6 +7,7 @@ RSpec.feature "Listing Exercises" do
 
     @e1 = @john.exercises.create(duration_in_min: 20, workout: "Bodybuilding activity", workout_date: Date.today)
     @e2 = @john.exercises.create(duration_in_min: 55, workout: "Weight lifting", workout_date: 2.days.ago)
+    @e3 = @john.exercises.create(duration_in_min: 35, workout: "Treadmill running", workout_date: 8.days.ago)
   end
 
   scenario "shows user's workout for last 7 days" do
@@ -21,6 +22,9 @@ RSpec.feature "Listing Exercises" do
     expect(page).to have_content(@e2.workout)
     expect(page).to have_content(@e2.workout_date)
 
+    expect(page).not_to have_content(@e3.duration_in_min)
+    expect(page).not_to have_content(@e3.workout)
+    expect(page).not_to have_content(@e3.workout_date)
   end
 
   scenario "shows no exercises if none exist" do
